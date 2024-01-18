f74c52672c11ab6579258b7cef325b0487e2026c
Marcio Woitek <mwoitekjr@gmail.com>
2024-01-18 02:15:23 -0300

    (feat) Online retail: Finish fixing dataset

--- customer_segmentation/online_retail.py
+++ customer_segmentation/online_retail.py
@@ -8,7 +8,9 @@
 from pathlib import Path
 from typing import cast
 
+import numpy as np
 import pandas as pd
+from pandas.testing import assert_frame_equal
 
 # %% [markdown]
 # ## Read dataset
@@ -80,3 +82,64 @@ assert df["InvoiceNo"].astype(str).str.startswith("C").sum() == 0, "there are re
 # %%
 df = df.loc[df["UnitPrice"] != 0.0, :]
 df = cast(pd.DataFrame, df)
+
+# %% [markdown]
+# ## Finish fixing dataset
+
+# %%
+df = df.reset_index(drop=True)
+df.info()
+
+# %%
+# Use appropriate data types
+df["InvoiceNo"] = df["InvoiceNo"].astype("category")
+df["CustomerID"] = df["CustomerID"].astype("category")
+df["Quantity"] = df["Quantity"].astype(np.int_)
+df["UnitPrice"] = df["UnitPrice"].astype(np.float_)
+
+df.dtypes
+
+# %% [markdown]
+# The only part of `InvoiceDate` that matters is the date. The following
+# command sets all the times to midnight:
+
+# %%
+df["InvoiceDate"] = df["InvoiceDate"].dt.normalize()
+df["InvoiceDate"].head()
+
+# %% [markdown]
+# For convenience, I'll collect the essential parts of the above code, and
+# create a function:
+
+
+# %%
+def get_clean_data(file_path: Path) -> pd.DataFrame:
+    cols = ["InvoiceNo", "InvoiceDate", "CustomerID", "Quantity", "UnitPrice"]
+    df = pd.read_excel(file_path, usecols=cols, dtype={col: object for col in cols}).loc[:, cols]
+    df = cast(pd.DataFrame, df)
+
+    df = df.dropna()
+
+    df = df.loc[df["Quantity"] > 0, :]
+    df = cast(pd.DataFrame, df)
+
+    df = df.loc[df["UnitPrice"] != 0.0, :]
+    df = cast(pd.DataFrame, df)
+
+    df = df.reset_index(drop=True)
+
+    df["InvoiceNo"] = df["InvoiceNo"].astype("category")
+    df["CustomerID"] = df["CustomerID"].astype("category")
+    df["Quantity"] = df["Quantity"].astype(np.int_)
+    df["UnitPrice"] = df["UnitPrice"].astype(np.float_)
+
+    df["InvoiceDate"] = df["InvoiceDate"].dt.normalize()
+
+    return df
+
+
+# %%
+# Quick check
+df_func = get_clean_data(file_path)
+assert_frame_equal(df_func, df)
+del df_func
