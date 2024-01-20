91808f6f10f23150c28cf9c9dd58fafd58748cc2
Marcio Woitek <mwoitekjr@gmail.com>
2024-01-20 01:09:47 -0300

    (feat) RFM ex.: Add func. for computing RFM scores

--- customer_segmentation/rfm_example.py
+++ customer_segmentation/rfm_example.py
@@ -8,7 +8,7 @@
 # ## Imports
 
 # %%
-from typing import Literal, cast
+from typing import Literal, cast, get_args
 
 import numpy as np
 import pandas as pd
@@ -195,3 +195,39 @@ df_rfm.loc[:, ["RFMCell", "RFMScore"]]
 # Notice that these results agree with the reference values, except for the
 # rows with `CustomerId` 7 and 13. As expected, the difference is in the F
 # score values.
+#
+# Now I'm confident that my implementation is correct. So I'll collect the
+# essential parts of the code above, and write a function that adds the RFM
+# scores to the original DataFrame.
+
+
+# %%
+def add_score_column(df: pd.DataFrame, attr: RFMAttribute, num_bins: int = 5) -> pd.DataFrame:
+    score_name = f"{attr[0]}Score"
+    score_labels = list(range(num_bins, 0, -1)) if attr == "Recency" else list(range(1, num_bins + 1))
+
+    rank = df[attr].rank(method="min").astype(np.int_)
+    df[score_name] = pd.cut(rank, num_bins, labels=score_labels)
+    if attr == "Recency":
+        df[score_name] = df[score_name].cat.reorder_categories(score_labels[::-1], ordered=True)
+
+    return df
+
+
+# %%
+def add_rfm_scores(df: pd.DataFrame, num_bins: int = 5) -> pd.DataFrame:
+    for attr in get_args(RFMAttribute):
+        df = add_score_column(df, attr, num_bins)
+
+    score_cols = [f"{attr[0]}Score" for attr in get_args(RFMAttribute)]
+    df["RFMCell"] = df[score_cols].agg(lambda r: f"{r.iloc[0]},{r.iloc[1]},{r.iloc[2]}", axis="columns")
+    df["RFMScore"] = df[score_cols].astype(np.int_).agg("mean", axis="columns")
+
+    return df
+
+
+# %%
+# Quick test
+df = add_rfm_scores(df)
+assert_frame_equal(df[["RFMCell", "RFMScore"]], df_rfm[["RFMCell", "RFMScore"]])
+df
