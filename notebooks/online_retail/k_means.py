07bb5e329d4ede5ff45bc033725e549e70bfdb61
Marcio Woitek <mwoitekjr@gmail.com>
2024-02-03 13:52:57 -0300

    (feat) k-means: First attempt

--- notebooks/online_retail/k_means.py
+++ notebooks/online_retail/k_means.py
@@ -11,6 +11,8 @@ from pathlib import Path
 
 import numpy as np
 import pandas as pd
+from pandas.testing import assert_series_equal
+from sklearn.cluster import KMeans
 
 # %% [markdown]
 # ## Read features
@@ -46,3 +48,77 @@ df.head()
 
 # %%
 df.info()
+
+# %% [markdown]
+# ## k-means: First attempt
+#
+# In this section, the goal is to get the basic code working. Improvements on
+# this initial approach will be made later. For this first attempt, we'll
+# **arbitrarily** choose `k = 5`.
+
+# %%
+# Use power transform features
+features = ["PTRecency", "PTFrequency", "PTAvgSpent"]
+X = df[features].to_numpy()
+X
+
+# %%
+# k-means parameters
+k = 5
+max_iter = 300
+
+# %%
+# Get cluster labels
+kmeans = KMeans(n_clusters=k, max_iter=max_iter, random_state=333)
+cluster_labels = kmeans.fit_predict(X)
+assert kmeans.n_iter_ < max_iter, f"maximum number of iterations is too low: {max_iter}"
+cluster_labels
+
+# %%
+# Unique labels
+unique_labels = np.unique(cluster_labels)
+np.sort(unique_labels)
+
+# %%
+# Make labels start from 1
+cluster_labels += 1
+unique_labels = np.unique(cluster_labels)
+np.sort(unique_labels)
+
+# %%
+del unique_labels
+
+# %%
+# Add column to DataFrame
+column_name = f"ClusterLabel{k}"
+df[column_name] = cluster_labels
+df.head()
+
+# %% [markdown]
+# The above code seems to be working correctly. Then we're going to collect the
+# essential parts of this code, and use them to define a function:
+
+
+# %%
+def add_cluster_labels(
+    df: pd.DataFrame,
+    features: list[str],
+    k: int,
+    max_iter: int = 300,
+) -> pd.DataFrame:
+    X = df[features].to_numpy()  # noqa: N806
+    kmeans = KMeans(n_clusters=k, max_iter=max_iter, random_state=333).fit(X)
+    assert kmeans.n_iter_ < max_iter, f"maximum number of iterations is too low: {max_iter}"
+    df[f"ClusterLabel{k}"] = kmeans.predict(X) + 1
+    return df
+
+
+# %%
+# Quick check
+old_labels = df.pop("ClusterLabel5")
+df = add_cluster_labels(
+    df,
+    features=["PTRecency", "PTFrequency", "PTAvgSpent"],
+    k=5,
+)
+assert_series_equal(df.ClusterLabel5, old_labels)
